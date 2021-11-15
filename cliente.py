88235a6856b20a9ff6247cb486deecc14e9281fc
Zino243 <67828167+Zino243@users.noreply.github.com>
2021-11-15 16:07:51 +0000

    client

---
+++ cliente.py
@@ -0,0 +1,10 @@
+import socket
+import subprocess
+
+mi_socket = socket.socket()
+mi_socket.connect( ('localhost', 8000) )
+
+
+respuesta = mi_socket.recv(1024)
+print(str(respuesta))
+mi_socket.close()
