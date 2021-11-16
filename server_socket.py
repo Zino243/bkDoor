461f0f29352a7f0f139591ccd049e9b8851bcb2d
Zino243 <67828167+Zino243@users.noreply.github.com>
2021-11-16 09:18:09 +0100

    Update server_socket.py

--- server_socket.py
+++ server_socket.py
@@ -1,7 +1,7 @@
 import socket
 import subprocess
 import optparse
-mi_socket = socket.socket()
+mi_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def get_arguments():
     parser = optparse.OptionParser()
@@ -13,20 +13,32 @@ def get_arguments():
     elif not options.port:
         parser.error("[-] Por favor indica un puerto, usa --help para mas informacion")
     return options
+
 def start_server(host, port):
     print("[+] iniciando servidor")
     port = (int(port))
-    mi_socket.bind( (host,port) )
+    mi_socket.connect( (host,port) )
     mi_socket.listen(5)
+
+def ejecutar_comando(command):
+    return subprocess.check_output( command, shell=True)
 
 options = get_arguments()
 start_server(options.host,options.port)
 
 
+conexion, addr = mi_socket.accept()
+print("[+] nueva conexion establecida")
+print(addr)
 while True:
-    conexion, addr = mi_socket.accept()
-    print("[+] nueva conexion establecida")
-    print(addr)
-    call = subprocess.call("ipconfig")
-    conexion.send(call)
-    conexion.close()
+    
+
+
+
+
+    inp = input("comando -> ")
+    call = subprocess.call(inp)
+    send_call = call.to_bytes(2, 'little')
+    conexion.send(send_call)
+    
+conexion.close()
