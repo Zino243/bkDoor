6e9bb6e1c3bb7c857f739e827624f6bb74511133
Zino243 <67828167+Zino243@users.noreply.github.com>
2022-03-04 21:14:41 +0100

    Create client.py

---
+++ client.py
@@ -0,0 +1,28 @@
+import socket
+
+mi_socket = socket.socket()
+mi_socket.connect( ('95.63.226.100', 8000) )
+
+i = True
+def disconnect():
+    print("se ha cerrado el programa")
+    mi_socket.close()
+    return i
+
+#escribe lo que el servidor le mande al conectarse a tamaño de 1024bits o menor
+#respuesta = mi_socket.recv(1024)
+#print(str(respuesta))
+
+while i:
+    rec = input("abrir servidor? (s/n): ")
+    print(rec)
+    if rec == "s":
+        rec = bytes(rec, 'utf-8')
+        mi_socket.send(rec)
+        print("solicitud enviada")
+        i = False
+        disconnect()
+    elif rec == "n":
+        i = False
+        disconnect()
+    else: print("no ha introducido correctamente")
